e859d9fe4002e9db1b5e9ddbab1cd38e5ea82596
JQ\jieqiang <jieqiangt@gmail.com>
2025-02-06 15:48:32 +0800

    - initailize blank nextjs project

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+import "./globals.scss";
+
+export const metadata = {
+  title: "My Expenses",
+  description: "Just an expense tracking application",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
